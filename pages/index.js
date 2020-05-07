3f9d94a8a9c8bd49a95dd824af573d911bf9107b
Yash Solanki <yashsolanki777@gmail.com>
2020-05-07 20:49:52 +0530

    react boilerplate integrated

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+// import factory from "../ethereum/factory";
+import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
+import { Link } from "../routes";
+
+class Index extends Component {
+    // static async getInitialProps() {}
+
+    render() {
+        return (
+            <Layout>
+                <h1>Title</h1>
+            </Layout>
+        );
+    }
+}
+
+export default Index;
